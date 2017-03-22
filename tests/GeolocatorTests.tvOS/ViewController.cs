02f28c9a172b2079f1771b7cca612e9c763954ff
James Montemagno <james.montemagno@gmail.com>
2017-03-22 15:52:46 -0700

    update tvOS sample

--- tests/GeolocatorTests.tvOS/ViewController.cs
+++ tests/GeolocatorTests.tvOS/ViewController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Foundation;
+using Plugin.Geolocator;
 using UIKit;
 
 namespace GeolocatorTests.tvOS
@@ -13,6 +15,48 @@ namespace GeolocatorTests.tvOS
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
+
+            ButtonAddress.AllEvents += async (sender, e) =>
+            {
+                try
+                {
+                    var position = await CrossGeolocator.Current.GetPositionAsync(TimeSpan.FromSeconds(15));
+                    var address = await CrossGeolocator.Current.GetAddressesForPositionAsync(position);
+
+                    var first = address.FirstOrDefault();
+                    LabelAddress.Text = $"Address: {first.Thoroughfare} {first.Locality}";
+                }
+                catch (Exception ex)
+                {
+                    LabelAddress.Text = "Error";
+                }
+            };
+
+
+            ButtonEnabled.AllEvents += async (sender, e) =>
+            {
+                LabelEnabled.Text = CrossGeolocator.Current.IsGeolocationEnabled ? "Enabled" : "Not Enabled";
+
+            };
+
+            ButtonAvailable.AllEvents += async (sender, e) =>
+            {
+                LabelAvailable.Text = CrossGeolocator.Current.IsGeolocationAvailable ? "Available" : "Not Available";
+
+            };
+
+            ButtonGetLocation.AllEvents += async (sender, e) =>
+            {
+                var test = await CrossGeolocator.Current.GetPositionAsync(TimeSpan.FromMinutes(2));
+                LabelLocation.Text = "Full: Lat: " + (test.Latitude).ToString() + " Long: " + (test.Longitude).ToString();
+                LabelLocation.Text += "\n" + $"Time: {test.Timestamp.ToString()}";
+                LabelLocation.Text += "\n" + $"Heading: {test.Heading.ToString()}";
+                LabelLocation.Text += "\n" + $"Speed: {test.Speed.ToString()}";
+                LabelLocation.Text += "\n" + $"Accuracy: {test.Accuracy.ToString()}";
+                LabelLocation.Text += "\n" + $"Altitude: {test.Altitude.ToString()}";
+                LabelLocation.Text += "\n" + $"AltitudeAccuracy: {test.AltitudeAccuracy.ToString()}";
+
+            };
             // Perform any additional setup after loading the view, typically from a nib.
         }
 
@@ -21,6 +65,8 @@ namespace GeolocatorTests.tvOS
             base.DidReceiveMemoryWarning();
             // Release any cached data, images, etc that aren't in use.
         }
+
+
     }
 }
 
