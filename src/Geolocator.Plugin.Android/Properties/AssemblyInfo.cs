91e406b6daf6b77bb15c9d18b53e0d1a4f3a84ca
James Montemagno <james.montemagno@gmail.com>
2017-03-28 14:04:39 +0100

    Fix typo in assemblyinfo Fixes #76

--- src/Geolocator.Plugin.Android/Properties/AssemblyInfo.cs
+++ src/Geolocator.Plugin.Android/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using Android.App;
 [assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
 [assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
 //Required when targeting Android API 21+
-[assembly: UsesFeature("android.feature.location.gps")]
-[assembly: UsesFeature("android.feature.location.network")]
+[assembly: UsesFeature("android.hardware.location.gps")]
+[assembly: UsesFeature("android.hardware.location.network")]
