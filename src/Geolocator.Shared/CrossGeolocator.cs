1e563de44dd8e9323321f95e2a4f03b11f4232e3
Allan Ritchie <allan.ritchie@gmail.com>
2017-06-04 12:21:18 -0400

    allow shared geolocator instance to be overwritten, move to shared project

---
+++ src/Geolocator.Shared/CrossGeolocator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Plugin.Geolocator.Abstractions;
+
+
+namespace Plugin.Geolocator
+{
+    /// <summary>
+    /// Cross platform Geolocator implemenations
+    /// </summary>
+    public class CrossGeolocator
+    {
+        static IGeolocator current;
+
+
+        /// <summary>
+        /// Current settings to use
+        /// </summary>
+        public static IGeolocator Current
+        {
+            get
+            {
+#if BAIT
+                if (current == null)
+                    throw new NotImplementedException("[Plugin.Geolocator] This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+#else
+                current = current ?? new GeolocatorImplementation();
+#endif
+                return current;
+            }
+            set => current = value;
+        }
+    }
+}
