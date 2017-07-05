5a220612b04e9484d8b36615d5b379e317ef20a0
James Montemagno <james.montemagno@gmail.com>
2017-07-05 14:30:21 -0700

    Update sample

--- samples/GeolocatorSample/GeolocatorSample/App.cs
+++ samples/GeolocatorSample/GeolocatorSample/App.cs
@@ -7,66 +7,66 @@ using Xamarin.Forms;
 
 namespace GeolocatorSample
 {
-    public class App : Application
-    {
-        public App()
-        {
+	public class App : Application
+	{
+		public App()
+		{
 
-            var buttonGetGPS = new Button
-            {
-                Text = "GetGPS"
-            };
+			var buttonGetGPS = new Button
+			{
+				Text = "GetGPS"
+			};
 
-            var labelGPS = new Label
-            {
-                Text = "GPS goes here"
-            };
+			var labelGPS = new Label
+			{
+				Text = "GPS goes here"
+			};
 
-            buttonGetGPS.Clicked += async (sender, args) =>
-            {
-                var locator = CrossGeolocator.Current;
-                locator.DesiredAccuracy = 50;
-                labelGPS.Text = "Getting gps";
+			buttonGetGPS.Clicked += async (sender, args) =>
+			{
+				var locator = CrossGeolocator.Current;
+				locator.DesiredAccuracy = 50;
+				labelGPS.Text = "Getting gps";
 
-                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+				var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10));
 
-                if (position == null)
-                {
-                    labelGPS.Text = "null gps :(";
-                    return;
-                }
-                labelGPS.Text = string.Format("Time: {0} \nLat: {1} \nLong: {2} \n Altitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \n Heading: {6} \n Speed: {7}",
-            position.Timestamp, position.Latitude, position.Longitude,
-            position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
-            };
+				if (position == null)
+				{
+					labelGPS.Text = "null gps :(";
+					return;
+				}
+				labelGPS.Text = string.Format("Time: {0} \nLat: {1} \nLong: {2} \n Altitude: {3} \nAltitude Accuracy: {4} \nAccuracy: {5} \n Heading: {6} \n Speed: {7}",
+				position.Timestamp, position.Latitude, position.Longitude,
+				position.Altitude, position.AltitudeAccuracy, position.Accuracy, position.Heading, position.Speed);
+			};
 
-            // The root page of your application
-            MainPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    VerticalOptions = LayoutOptions.Center,
-                    Children = {
-                        buttonGetGPS,
-            labelGPS
-                    }
-                }
-            };
-        }
+			// The root page of your application
+			MainPage = new ContentPage
+			{
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						buttonGetGPS,
+						labelGPS
+					}
+				}
+			};
+		}
 
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
 
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
 
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
-        }
-    }
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
 }
