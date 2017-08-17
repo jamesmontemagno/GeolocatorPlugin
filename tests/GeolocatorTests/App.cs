132719fc57301c8d8f44b97c8b8f9782c3ebe7d9
James Montemagno <james.montemagno@gmail.com>
2017-08-16 17:55:41 -0700

    Add geocoder tests

---
+++ tests/GeolocatorTests/App.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GeolocatorTests
+{
+	public class App : Application
+	{
+		public App()
+		{
+
+			// The root page of your application
+			MainPage = new NavigationPage(new HomePage());
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
