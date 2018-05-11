985d1a3133e5ad99197b809c48c95277b52c7f9f
James Montemagno <james.montemagno@gmail.com>
2018-05-11 16:31:33 -0700

    Add manifest info

---
+++ src/Geolocator.Plugin/Android/ManifestInfo.android.cs
@@ -0,0 +1,7 @@
+﻿using Android.App;
+
+[assembly: UsesPermission(Android.Manifest.Permission.AccessCoarseLocation)]
+[assembly: UsesPermission(Android.Manifest.Permission.AccessFineLocation)]
+//Required when targeting Android API 21+
+[assembly: UsesFeature("android.hardware.location.gps")]
+[assembly: UsesFeature("android.hardware.location.network")]
